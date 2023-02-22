d4ac22fdcd556bdd9305421c06e7c950ac5a7874
tejender <kumartejender007@gmail.com>
2023-02-22 21:55:21 +0530

    added cat list

--- staticfiles/js/swiper-cat-list.js
+++ staticfiles/js/swiper-cat-list.js
@@ -1,13 +1,16 @@
 var width = window.innerWidth;
 if (width<=500){
   var slideCount = 4
+  var gap=30
 }
 if(width>500){
   var slideCount = 9
+  var gap=60
 }
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: slideCount,
-  spaceBetween: 60,
+  spaceBetween: gap,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
