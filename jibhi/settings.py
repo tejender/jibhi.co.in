d078a2ae3d8cca4f2d4c8f22a126230232fb1dee
tejender <kumartejender007@gmail.com>
2023-02-23 20:03:19 +0530

    added files media

--- jibhi/settings.py
+++ jibhi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z67+=5ci3u9*dlmfrw^27m#)_px#=rbi=_h3z549bm!dlla0j$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['jibhicoin-production.up.railway.app/','127.0.0.1']
 
 
 # Application definition
