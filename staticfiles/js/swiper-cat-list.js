4cec24744b708e587dee97b7ad5fcad975a603b4
tejender <kumartejender007@gmail.com>
2023-02-23 12:15:41 +0530

    added cat list 3

--- staticfiles/js/swiper-cat-list.js
+++ staticfiles/js/swiper-cat-list.js
@@ -1,18 +1,22 @@
 var width = window.innerWidth;
 if (width<=600){
   var slideCount = 4
-  var gap=30
+  var gap=20
 }
 if(width>500){
-  var slideCount = 9
-  var gap=60
+  var slideCount = 7
+  var gap=30
 }
 
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
+  slidesPerView: slideCount,
+  spaceBetween: gap,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 });
