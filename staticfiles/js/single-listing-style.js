e65fc94c2db5ac505369ebee1851609875dde525
tejender <kumartejender007@gmail.com>
2023-03-16 11:13:30 +0530

    added enq

--- staticfiles/js/single-listing-style.js
+++ staticfiles/js/single-listing-style.js
@@ -58,26 +58,51 @@ closeGalleryBtn.addEventListener('click', function() {
 
 // listing gallery ends
 
-// datepicker
+//////////////////////////// datepicker desktop
 
-// Get the check-in and check-out date input elements
-const checkinInput = document.getElementById('checkin-date');
-const checkoutInput = document.getElementById('checkout-date');
+// Get the checkin and checkout date input elements
+const checkinInput = document.querySelector('#enquiry-form-desktop #checkin-date');
+const checkoutInput = document.querySelector('#enquiry-form-desktop #checkout-date');
 
-// Set the minimum check-in and check-out dates to the current date
-const currentDate = new Date().toISOString().split('T')[0];
-checkinInput.min = currentDate;
-checkoutInput.min = currentDate;
 
-// When the check-in date changes, set the minimum checkout date to the check-in date
+// Set the minimum checkin date to the current date
+checkinInput.min = new Date().toISOString().split('T')[0];
+
+// When the checkin date changes, set the minimum checkout date to the checkin date
 checkinInput.addEventListener('change', (event) => {
   checkoutInput.min = event.target.value;
 });
 
-// When the checkout date changes, set the maximum check-in date to the checkout date
+// When the checkout date changes, set the maximum checkin date to the checkout date
 checkoutInput.addEventListener('change', (event) => {
   checkinInput.max = event.target.value;
-});// datepicker ends
+});
+
+// datepicker ends
+
+
+
+// datepicker mobile
+// Get the checkin and checkout date input elements
+const checkinInputMobile = document.querySelector('#enquiry-modal-content #checkin-date');
+const checkoutInputMobile = document.querySelector('#enquiry-modal-content #checkout-date');
+
+
+// Set the minimum checkin date to the current date
+checkinInputMobile.min = new Date().toISOString().split('T')[0];
+
+// When the checkin date changes, set the minimum checkout date to the checkin date
+checkinInputMobile.addEventListener('change', (event) => {
+  checkoutInputMobile.min = event.target.value;
+});
+
+// When the checkout date changes, set the maximum checkin date to the checkout date
+checkoutInputMobile.addEventListener('change', (event) => {
+  checkinInputMobile.max = event.target.value;
+});
+
+// datepicker mobile ends
+
 
 
 // window.addEventListener("scroll", function() {
